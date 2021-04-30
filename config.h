ee7d437ee011175fa5bd6c1ec6e5792a9d096d99
kailabeam <80266800+kailabeam@users.noreply.github.com>
2021-04-30 16:50:07 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -107,7 +107,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -131,7 +131,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
+	"#151515", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
 
